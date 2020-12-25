fdeb81f72bbceb4be48844e70457e345e8651ff1
Molly Bjoerk <molly.k.bjork@gmail.com>
2020-12-25 08:44:07 +0100

    day2 done

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,42 @@
+use std::{alloc::Layout, collections::btree_map::Keys};
+
+fn part1() {
+    let file = std::fs::read_to_string("./src/input").unwrap();
+    let s = file
+        .lines()
+        .filter(|line| {
+            let split = line.split(' ').collect::<Vec<&str>>();
+            let a = split.first().unwrap().split('-').collect::<Vec<_>>();
+            let b = a.first().unwrap().parse::<u8>().unwrap();
+            let c = a.last().unwrap().parse::<u8>().unwrap();
+            let x = split[1].chars().next().unwrap();
+            let u = split.last().unwrap().chars().filter(|e| *e == x).count() as u8;
+            b <= u && c >= u
+        })
+        .count();
+    println!("{:?}", s);
+} 
+
+fn part2() {
+    let file = std::fs::read_to_string("./src/input").unwrap();
+    let s = file
+        .lines()
+        .filter(|line| {
+            let split = line.split(' ').collect::<Vec<&str>>();
+            let a = split.first().unwrap().split('-').collect::<Vec<_>>();
+            let pos1 = a.first().unwrap().parse::<u8>().unwrap();
+            let pos2 = a.last().unwrap().parse::<u8>().unwrap();
+            let x = split[1].chars().next().unwrap();
+            let u = split.last().unwrap().chars().collect::<Vec<char>>();
+            let ca = u[( pos1 - 1 ) as usize];
+            let cb = u[( pos2 - 1 ) as usize];
+
+            ca == x && cb != x || ca != x && cb == x
+        })
+        .count();
+    println!("{:?}", s);
+} 
+
 fn main() {
-    println!("Hello, world!");
+    part2();
 }
---
+++ day2/target/debug/deps/day2.d
@@ -0,0 +1,5 @@
+C:\Users\molly\Source\aoc2020\day2\target\debug\deps\day2.exe: src\main.rs
+
+C:\Users\molly\Source\aoc2020\day2\target\debug\deps\day2.d: src\main.rs
+
+src\main.rs:
